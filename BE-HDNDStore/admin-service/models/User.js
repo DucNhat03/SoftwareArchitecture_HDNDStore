2f8dc626265e17013d9a2417377d2d09048904b1
DucNhat <cuem8h@gmail.com>
2025-04-04 18:05:28 +0700

    [update]: update all

--- BE-HDNDStore/admin-service/models/User.js
+++ BE-HDNDStore/admin-service/models/User.js
@@ -1,28 +1,34 @@
-const mongoose = require("mongoose");
+import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
+const UserSchema = new mongoose.Schema(
+  {
     phone: { type: String, unique: true, sparse: true },
     password: { type: String },
     email: { type: String, required: true, unique: true },
-    googleId: { type: String, unique: true, sparse: true }, // Thêm Google ID
-    provider: { type: String, default: "local" }, // Xác định phương thức đăng nhập (local/google)
+    googleId: { type: String, unique: true, sparse: true },
+    provider: { type: String, default: "local" },
     fullName: { type: String, default: "" },
     gender: { type: String, default: "other" },
-    birthday: { 
-        day: { type: String, default: "" },
-        month: { type: String, default: "" },
-        year: { type: String, default: "" }
+    birthday: {
+      day: { type: String, default: "" },
+      month: { type: String, default: "" },
+      year: { type: String, default: "" },
     },
-    address: { 
-        city: { type: String, default: "" },
-        district: { type: String, default: "" },
-        ward: { type: String, default: "" },
-        street: { type: String, default: "" }
+    address: {
+      city: { type: String, default: "" },
+      district: { type: String, default: "" },
+      ward: { type: String, default: "" },
+      street: { type: String, default: "" },
     },
+    role: { type: String, default: "user" },
     avatar: { type: String, default: "" },
-}, { timestamps: true });
 
-const User = mongoose.model("User", UserSchema);
+    // Thêm trường OTP và xác thực OTP
+    otp: { type: String },
+    otpExpires: { type: Date },
+    isOtpVerified: { type: Boolean, default: false }, 
+  },
+  { timestamps: true }
+);
 
-module.exports = User;
+export default mongoose.model("Users", UserSchema);
