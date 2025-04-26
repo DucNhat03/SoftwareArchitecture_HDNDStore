8dfd3a9dbc379d70062c611717cd9108f5d1a4a7
DucNhat <cuem8h@gmail.com>
2025-04-26 19:13:24 +0700

    BE: init chat box

---
+++ FE-HDNDStore/hdndstore-fe/src/components/chat/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Header = ({ toggled, setToggled }) => {
+  return (
+    <div className="chat-header">
+      <h1 className='header-layout'>{'< ducnhatdev />'}</h1>
+      <span className='toggle-text'>Stream Response</span>
+      <button 
+        className={`toggle-btn ${toggled ? "toggled": ""}`}
+        onClick={() => setToggled(!toggled)}
+      >
+        <div className="toggle-hover">
+          <div className='thumb'></div>
+          {toggled === false ? (
+            <span className="toggle-hover-text">Streaming response Off</span>
+          ) : (
+            <span className="toggle-hover-text">Streaming response On</span>
+          )}
+        </div>
+      </button>
+    </div>
+  );
+};
+
+
+export default Header;
---
+++ FE-HDNDStore/hdndstore-fe/src/components/chat/MessageInput.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
+
+/** Submission using the Enter key or button. */
+const MessageInput = ({ inputRef, waiting, handleClick }) => {
+  return (
+    <div className="message-input">
+      <input
+        className="chat_msg_input"
+        type="text"
+        name="chat"
+        placeholder={waiting ? "Waiting for response..." : "Type a message..."}
+        ref={inputRef}
+        disabled={waiting}
+        onKeyDown={(e) => {
+          if (e.key === "Enter") handleClick();
+        }}
+      />
+      <button 
+        className="chat_msg_btn" 
+        onClick={handleClick}
+        disabled={waiting}
+      >
+        <span className="fa-span-send">
+          <FontAwesomeIcon icon={faPaperPlane} />
+        </span>
+      </button>
+    </div>
+  );
+};
+
+export default MessageInput;
