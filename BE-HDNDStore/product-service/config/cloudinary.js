0abbfce15dac24c0af9d77157158095d2cf8f3ee
tran quoc huy <tranquochuy15042003@gmail.com>
2025-04-26 18:49:47 +0700

    them gitignore

--- BE-HDNDStore/product-service/config/cloudinary.js
+++
@@ -1,9 +0,0 @@
-import { v2 as cloudinary } from 'cloudinary';
-
-cloudinary.config({
-    cloud_name: "df5hmhosz",
-    api_key: "185815117947463",
-    api_secret: "El1eVyq38XB0pZR6xmQ7-gjsAfI",
-});
-
-export default cloudinary;
