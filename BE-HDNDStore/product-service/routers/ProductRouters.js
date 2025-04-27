8e195bf599dbd3c092cf1ddfe7a297b2e578bca7
DucNhat <cuem8h@gmail.com>
2025-04-27 14:57:08 +0700

    [BE]: update product services (search by param)

--- BE-HDNDStore/product-service/routers/ProductRouters.js
+++ BE-HDNDStore/product-service/routers/ProductRouters.js
@@ -8,4 +8,9 @@ router.get("/all", productController.getAllProducts);
 // Route lọc sản phẩm theo category
 router.get("/all/category", productController.getProductsByCategory);
 
+router.get("/all/:category", productController.getProductsByCategoryy);
+
+// Route lấy sản phẩm đồng giá
+router.get("/all/price/:price", productController.getProductsByPrice);
+
 module.exports = router;
